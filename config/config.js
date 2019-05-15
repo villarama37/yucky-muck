1ce2a71affa67ffffa3f690b42d4f517f685afaa
melodysue <melody@science37.com>
2019-05-15 23:29:43 +0000

    Cleaned up config to include only necessary attributes

--- config/config.js
+++ config/config.js
@@ -4,19 +4,9 @@ const config = {};
 
 // default is dev
 config.dev = {
-  notificationsFeatureEnabled: true,
   hapiOptions: {
     host: '0.0.0.0',
     port: 11116,
-  },
-  notificationDB: {
-    host: 'db-nora-dev.cti10lnrh4rb.us-west-2.rds.amazonaws.com',
-    user: 'admin',
-    port: 3306,
-    password: 'n0r4123!',
-    database: 'notification',
-    ssl: 'Amazon RDS',
-    connectionLimit: 10,
   },
   coreDB: {
     host: 'db-nora-dev.cti10lnrh4rb.us-west-2.rds.amazonaws.com',
@@ -32,13 +22,6 @@ config.dev = {
     host: 'localhost',
     port: 3005,
     prefix: '/api',
-  },
-  email: {
-    aws: {
-      region: 'us-west-2',
-    },
-    noreplyAddress: 'no-reply@science37.com',
-    loginAddress: 'https://dev.nora.science37.com',
   },
   logging: {
     aws: {
@@ -65,40 +48,22 @@ config.dev = {
       },
     },
   },
-  auditLog: {
-    aws: {
-      auditHistoryTable: 'NORAAuditHistoryDEV',
-    },
-  },
-  permissions: {
-    aws: {
-      region: 'us-west-2',
-      secretId: 'dev/ApiGateway/JwtRsaPublicKey',
-    },
-  },
-  toggles: {
-    features: [
-      {
-        key: 'QUERY_MANAGEMENT',
-        name: 'Query Management',
-        version: '2.14.0',
-        description: 'https://science37.atlassian.net/browse/TE-1061',
-      },
-    ],
-  },
-  cache: {
-    defaultScheme: 'lru',
-    defaultMaxAge: 15 * 60 * 1000,
-    defaultMax: 500,
+  aws: {
+    region: 'us-west-2',
+    auditHistoryTable: 'NORAAuditHistoryDEV',
+    auditHistoryMetaTable: 'NORAAuditHistoryMetaDEV',
+    s3LargeHistoryBucket: 'nora-dev-private',
+    elasticSearch: {
+      host: 'https://search-audit-history-dev-qjugiqgkm7w2fqf2btukeoshpu.us-west-2.es.amazonaws.com',
+      log: 'trace',
+      index: 'auditlog',
+      type: 'entries',
+    }
   },
 }
 
 // bi-ecs
 config.bi_ecs = {
-  notificationDB : {
-    host: 'db-nora-bi.science37.com',
-    database: 'notification-bi',
-  },
   coreDB: {
     host: 'db-nora-bi.science37.com',
     database: 'nora-bi',
@@ -106,9 +71,6 @@ config.bi_ecs = {
   noraCore: {
     host: 'monolith',
   },
-  email: {
-    loginAddress: 'https://bi-sandbox.nora.science37.com',
-  },
   logging: {
     aws: {
       logGroup: '/bi-ecs/nora/notifications/',
@@ -126,37 +88,16 @@ config.bi_ecs = {
       },
     },
   },
-  auditLog: {
-    aws: {
-      auditHistoryTable: 'NORAAuditHistoryBI',
-    },
-  },
-  toggles: {
-    features: [
-      {
-        key: 'QUERY_MANAGEMENT',
-        name: 'Query Management',
-        version: '99.99.99',
-        description: 'https://science37.atlassian.net/browse/TE-1061',
-      },
-    ],
-  },
 }
 
 // build_ecs
 config.build_ecs = {
-  notificationDB: {
-    host: 'db-nora-build.science37.com',
-  },
   coreDB: {
     host: 'db-nora-build.science37.com',
   },
   noraCore: {
     host: 'monolith',
   },
-  email: {
-    loginAddress: 'https://build.nora.science37.com',
-  },
   logging: {
     aws: {
       logGroup: '/bi-ecs/nora/notifications/',
@@ -172,11 +113,6 @@ config.build_ecs = {
       eventlog: {
         'default': 'warn',
       },
-    },
-  },
-  auditLog: {
-    aws: {
-      auditHistoryTable: 'NORAAuditHistoryBUILD',
     },
   },
 }
