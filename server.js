f22ad08a5ebc8ca5162c21c116841a032803763a
garrettv11 <garrett.y.kim@gmail.com>
2019-05-22 17:48:45 +0000

    [SAAS-88] linted server file

--- server.js
+++ server.js
@@ -6,33 +6,35 @@ const Pack = require('./package');
 const exampleRoutes = require(__dirname + '/src/routes/v1/MyModel.js');
 
 (async () => {
-    const server = await new Hapi.Server({
-        host: 'localhost',
-        port: 4044,
-    });
-    
-    const swaggerOptions = {
-        info: {
-                title: 'Test API Documentation',
-                version: Pack.version,
-            },
-        };
-    
-    await server.register([
-        Inert,
-        Vision,
-        {
-            plugin: HapiSwagger,
-            options: swaggerOptions
-        }
-    ]);
-    
-    try {
-        await server.start();
-        console.log('Server running at:', server.info.uri);
-    } catch(err) {
-        console.log(err);
-    }
-    
-    server.route(exampleRoutes);
+  const server = await new Hapi.Server({
+    host: 'localhost',
+    port: 4044,
+  });
+
+  const swaggerOptions = {
+    info: {
+      title: 'Test API Documentation',
+      version: Pack.version,
+    },
+  };
+
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions,
+    },
+  ]);
+
+  try {
+    await server.start();
+    console.log('Server running at:', server.info.uri);
+    console.log('Swagger definition available at:', server.info.uri + '/swagger.json');
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+  server.route(exampleRoutes);
 })();
