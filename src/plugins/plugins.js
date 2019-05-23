8918ef50c387dd5d42e97edd06fd829e4b96124f
garrettv11 <garrett.y.kim@gmail.com>
2019-05-23 20:27:34 +0000

    [SAAS-88] linted plugins file added start-dev script

--- src/plugins/plugins.js
+++ src/plugins/plugins.js
@@ -4,17 +4,17 @@ const HapiSwagger = require('hapi-swagger');
 const Pack = require('../../package');
 
 const plugins = [
-    Inert,
-    Vision,
-    {
-      plugin: HapiSwagger,
-      options: {
-       info: {
-          title: 'Test API Documentation',
-          version: Pack.version,
-        },
+  Inert,
+  Vision,
+  {
+    plugin: HapiSwagger,
+    options: {
+      info: {
+        title: 'Test API Documentation',
+        version: Pack.version,
       },
-    }
+    },
+  },
 ];
 
 module.exports = plugins;
