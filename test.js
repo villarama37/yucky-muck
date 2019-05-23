80ffd8e6e361e4d2adf103e09248bd40cb8477f6
melodysue <melody@science37.com>
2019-05-23 21:18:20 +0000

    [ARCH-95] Updated config file to default to local cloud9 and added additional environments

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const _ = require('lodash');
 
-const config = require('./config/config.js');
+const config = require('./config.js');
 
-const defaultConfig = config.dev;
+const defaultConfig = config.local;
 const environment = process.env.NODE_ENV || 'dev';
 const environmentConfig = config[environment];
 const finalConfig = _.merge(defaultConfig, environmentConfig);
