4fc42ef37bcb087afde2090552fb02c8e392826a
Garrett Kim <garrett.kim@science37.com>
2019-06-03 15:39:37 -0700

    [SAAS-85] added password to setup sql file

--- migrations/create-schema.sql
+++ migrations/create-schema.sql
@@ -5,5 +5,5 @@ Deployed versions should not assign a password to the starter kit user
 They should assign no password and use IAM authentication instead 
 */
 
-GRANT ALL PRIVILEGES ON `starter-kit`.* TO 'starter-kit'@'localhost';
-GRANT ALL PRIVILEGES ON `starter-kit`.* TO 'starter-kit'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `starter-kit`.* TO 'starter-kit'@'localhost' IDENTIFIED BY '3k293cp0tjnMq';
+GRANT ALL PRIVILEGES ON `starter-kit`.* TO 'starter-kit'@'%' IDENTIFIED BY '3k293cp0tjnMq' WITH GRANT OPTION;
