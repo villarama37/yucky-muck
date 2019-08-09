f0f41846b5ae110d99748416c2c8def25746440c
garrettv11 <garrett.y.kim@gmail.com>
2019-08-09 21:51:55 +0000

    [XX-000] added ability to execute from lambda function

---
+++ lambda.index.js
@@ -0,0 +1,27 @@
+const config = require(__dirname + '/config.js');
+const server = require(__dirname + '/server.js');
+
+exports.handler = async (requestOptions) => {
+  console.log(__dirname + '/config.js');
+  console.log(__dirname + '/server.js');
+  const request = {
+    method: requestOptions.method,
+    url: `http://${config.hapiOptions.host}:${config.hapiOptions.port}${requestOptions.path}`,
+    payload: requestOptions.payload,
+    validate: false,
+  };
+  let hapiServer;
+  try {
+    hapiServer = await server.start();
+    const { result } = await hapiServer.inject(request);
+    await hapiServer.stop();
+    return result;
+  }
+  catch (error) {
+    if (hapiServer) {
+      await hapiServer.stop();
+    }
+    console.log('studio lambda error is :', error);
+    throw error;
+  }
+};
