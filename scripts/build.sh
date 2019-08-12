7be080abf6ab3b5c857e133bac91d6240fd75147
villarama37 <38706757+villarama37@users.noreply.github.com>
2019-08-12 10:00:54 -0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 
 #
-# We want the lambda zip file to be as small as possible.  We also don't want to add files accidentally specally the .env files
+# We want the lambda zip file to be as small as possible.  We also don't want to add files accidentally specially the .env files
 # that could contain passwords.
 # 
 # This script copies only the required files in the build directory.
