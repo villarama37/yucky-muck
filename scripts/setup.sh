f91c58860bf66094cfb929f9a1b54cbe7cdab689
melodysue <melody@science37.com>
2019-09-05 18:38:54 +0000

    [XX-000] Listed items to change for setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,12 @@
+# Hapi-starter-kit update list
+# change service port number
+# change service name in config
+# supply new database name if needed and update in config
+# replace log group names
+# update database.json and database migration files
+# update name and description in package.json
+# update SAM template: description, resource name, function name, and stage name
+# rename title in hapi swagger plugin
+
+
+# 
